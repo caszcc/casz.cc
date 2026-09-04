707140dae2ea0ee524f6fd305572cb93cc645123
caszcc <casz@aster.cx>
2026-09-04 10:30:26 -0500

    exportado de framer - primer commit

---
+++ scripts/vendor/I7ghIlXDjRdUG5hyCbPpqvsZihZ-cJpz9rq0AxyDFdo.ChKsh2aA.mjs
@@ -0,0 +1,3 @@
+import { a as e, c as t, o as n, s as r } from "./shared-lib.g_c2uSJ3.mjs";
+r();
+export { e as __FramerMetadata__, n as getSnippets, t as snippetsSorting };
---
+++ scripts/vendor/chunk-6WHHSJFS.mjs
@@ -0,0 +1,3 @@
+import { b as r } from "./chunk-4RYFLPU3.mjs";
+r();
+//# sourceMappingURL=https://app.framerstatic.com/chunk-6WHHSJFS.mjs.map
